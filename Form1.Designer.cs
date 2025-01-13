c446341242575e0c9c00a7e31c98849c6726711d
Artyom <146100356+arutimasu@users.noreply.github.com>
2025-01-13 13:40:46 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.file_path = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.new_filepath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.переименоватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.получитьИнформциюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.rows = new System.Windows.Forms.TextBox();
+            this.columns = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(297, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(705, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // file_path
+            // 
+            this.file_path.Location = new System.Drawing.Point(14, 57);
+            this.file_path.Name = "file_path";
+            this.file_path.ReadOnly = true;
+            this.file_path.Size = new System.Drawing.Size(274, 20);
+            this.file_path.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(171, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Текущая директория:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(620, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Переименовать/Переместить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // new_filepath
+            // 
+            this.new_filepath.Location = new System.Drawing.Point(378, 56);
+            this.new_filepath.Name = "new_filepath";
+            this.new_filepath.Size = new System.Drawing.Size(236, 20);
+            this.new_filepath.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(294, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Новое расп-е:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.переименоватьToolStripMenuItem,
+            this.удалитьToolStripMenuItem,
+            this.получитьИнформциюToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // переименоватьToolStripMenuItem
+            // 
+            this.переименоватьToolStripMenuItem.Name = "переименоватьToolStripMenuItem";
+            this.переименоватьToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.переименоватьToolStripMenuItem.Text = "Переименовать";
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.удалитьToolStripMenuItem.Text = "Удалить";
+            this.удалитьToolStripMenuItem.Click += new System.EventHandler(this.удалитьToolStripMenuItem_Click);
+            // 
+            // получитьИнформциюToolStripMenuItem
+            // 
+            this.получитьИнформциюToolStripMenuItem.Name = "получитьИнформциюToolStripMenuItem";
+            this.получитьИнформциюToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.получитьИнформциюToolStripMenuItem.Text = "Получить информцию";
+            this.получитьИнформциюToolStripMenuItem.Click += new System.EventHandler(this.получитьИнформциюToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(620, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Выбрать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "*.txt",
+            "*.png",
+            "*.jpg"});
+            this.comboBox1.Location = new System.Drawing.Point(14, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 21);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.Text = "Текущий формат";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // rows
+            // 
+            this.rows.Location = new System.Drawing.Point(152, 88);
+            this.rows.Name = "rows";
+            this.rows.Size = new System.Drawing.Size(57, 20);
+            this.rows.TabIndex = 16;
+            this.rows.Text = "200";
+            // 
+            // columns
+            // 
+            this.columns.Location = new System.Drawing.Point(231, 88);
+            this.columns.Name = "columns";
+            this.columns.Size = new System.Drawing.Size(57, 20);
+            this.columns.TabIndex = 17;
+            this.columns.Text = "200";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 114);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(778, 475);
+            this.flowLayoutPanel1.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Размер плиток:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(215, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "x";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 601);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.columns);
+            this.Controls.Add(this.rows);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.new_filepath);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.file_path);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Tile Manager";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox file_path;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox new_filepath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem переименоватьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem получитьИнформциюToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox rows;
+        private System.Windows.Forms.TextBox columns;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string tmp = "";
+        string selectedState = "*.txt";
+        //string[] files;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            //tableLayoutPanel1.ColumnCount = 5;
+            //tableLayoutPanel1.RowCount = 5;
+            textBox1.Text = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            LoadFiles(System.IO.Path.GetDirectoryName(Application.ExecutablePath), "*.txt");
+           
+        }
+        void LoadFiles(string dir, string ext)
+        {
+            //tableLayoutPanel1.RowCount = Int32.Parse(rows.Text);
+            //tableLayoutPanel1.ColumnCount = Int32.Parse(columns.Text);
+
+            //foreach (RowStyle i in tableLayoutPanel1.RowStyles)
+            //{
+            //    i.SizeType = SizeType.Percent;
+            //    i.Height =  Int32.Parse(size.Text);
+            //}
+            //    foreach (ColumnStyle i in tableLayoutPanel1.ColumnStyles)
+            //    {
+            //        i.SizeType = SizeType.Percent;
+            //        i.Width  = Int32.Parse(size.Text);
+            //    }
+
+
+
+            //tableLayoutPanel1.Controls.Clear();
+            flowLayoutPanel1.Controls.Clear();
+            string[] files = Directory.GetFiles(dir, ext);
+            //int x = 0;
+            //int y = 0;
+            foreach (string file in files)
+            {
+                //if (x > tableLayoutPanel1.ColumnCount)
+                //{
+                //    x = 0;
+                //    y++;
+                //}
+                //tmp = file;
+                Button saveButton = new Button();
+                saveButton.Height = Int32.Parse(rows.Text);
+                saveButton.Width = Int32.Parse(columns.Text);
+                //if (file.Split(new char[] { '.' })[1]=="txt")
+                //{
+
+                if (ext == "*.png" || ext == "*.jpg")
+                    saveButton.Image = Image.FromFile(file);
+                    
+
+                else
+                {
+                    string readText = File.ReadAllText(file);
+                    saveButton.Text = readText;
+                    saveButton.Image = TileManager.Properties.Resources.leaf_3;
+                }
+                // добавляем кнопку в следующую свободную ячейку
+                saveButton.Click += (s, e) =>
+                {
+                    System.Diagnostics.Process.Start(file);
+                };
+                
+                saveButton.Enter += (s, e) =>
+                {
+                    this.Text = $"Tile Manager - {Path.GetFileName(file)}";
+                    file_path.Text = file;
+                };
+                //saveButton.MouseEnter += (s, e) =>
+                //{
+                //    this.Text = $"Form1 - {Path.GetFileName(file)}";
+                //    file_path.Text = file;
+                //};
+                //C:\Users\Пользователь\Desktop
+                //tableLayoutPanel1.Controls.Add(saveButton, x, y);
+                flowLayoutPanel1.Controls.Add(saveButton);
+                //x++;
+                //tableLayoutPanel1.SetRowSpan(saveButton, 1);
+
+                // Open the file to read from.
+                // }
+
+            }
+        }
+        public bool HasBinaryContent(string content)
+        {
+            return content.Any(ch => char.IsControl(ch) && ch != '\r' && ch != '\n');
+        }
+
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //files = Directory.GetFiles(textBox1.Text, "*.txt");
+            LoadFiles(textBox1.Text,selectedState);
+        }
+
+    
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            System.IO.File.Move(file_path.Text, new_filepath.Text);
+            LoadFiles(textBox1.Text,selectedState);
+        }
+
+   
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(file_path.Text);
+        }
+
+        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            File.Delete(file_path.Text);
+            LoadFiles(textBox1.Text,selectedState);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Show the FolderBrowserDialog.
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+               
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedState = comboBox1.SelectedItem.ToString();
+            //MessageBox.Show(selectedState);
+        }
+
+        private void получитьИнформциюToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileInfo info = new FileInfo(file_path.Text);
+            MessageBox.Show($"Имя: {info.Name}\nРазмер: {info.Length} байт\n Время создания: {info.CreationTime}");
+        }
+
+    
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
